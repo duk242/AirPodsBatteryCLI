e61cbd7932cb1e527b4f8f3be51b752b10beb85a
DanozDotNet <danoz@danoz.net>
2017-07-23 22:58:51 +1000

    final cleanup

--- AirPodsPower.sh
+++ AirPodsPower.sh
@@ -5,28 +5,29 @@
 
 # Put the Mac Address of your W1-enabled headphones (Apple AirPods, Beats Solo3, Powerbeats3, BeatsX) in here.
 MACADDR='7c-04-d0-af-88-62'
+ICON='\xf0\x9f\x8e\xa7'
 
 # See if we're connected to them
 CONNECTED=$(system_profiler SPBluetoothDataType|awk "/${MACADDR}/i {for(i=1; i<=6; i++) {getline; print}}"|grep "Connected: Yes"|sed 's/.*Connected: Yes/1/')
 if [[ "${CONNECTED}" ]]; then
-  BTDATA=$(defaults read /Library/Preferences/com.apple.Bluetooth|awk "/\"${MACADDR}\".=\s*\{[^\}]*\}/i {for(i=1; i<=6; i++) {getline; print}}")
+  BTDATA=$(defaults read /Library/Preferences/com.apple.Bluetooth|awk '/\"${MACADDR}\".=\s*\{[^\}]*\}/i {for(i=1; i<=6; i++) {getline; print}}')
 
   COMBINEDBATT=$(grep "BatteryPercentCombined" <<< "${BTDATA}"|sed 's/.*BatteryPercentCombined = //'|sed 's/;//')
   HEADSETBATT=$(grep "HeadsetBattery" <<< "${BTDATA}"|sed 's/.*HeadsetBattery = //'|sed 's/;//')
   SINGLEBATT=$(grep "BatteryPercentSingle" <<< "${BTDATA}"|sed 's/.*BatteryPercentSingle = //'|sed 's/;//')
   CASEBATT=$(grep "BatteryPercentCase" <<< "${BTDATA}"|sed 's/.*BatteryPercentCase = //'|sed 's/;//')
-  LEFTBATT=$(grep "BatteryPercentLeft" <<< "${BTDATA}"|sed 's/.*BatteryPercentLeft = //'|sed 's/;//') 
+  LEFTBATT=$(grep "BatteryPercentLeft" <<< "${BTDATA}"|sed 's/.*BatteryPercentLeft = //'|sed 's/;//')
   RIGHTBATT=$(grep "BatteryPercentRight" <<< "${BTDATA}"|sed 's/.*BatteryPercentRight = //'|sed 's/;//')
 
-  OUTPUT="🎧"
+  OUTPUT="${ICON}"
   [[ ! -z "${COMBINEDBATT}" ]] && OUTPUT="${OUTPUT} ${COMBINEDBATT}%"
   [[ ! -z "${HEADSETBATT}" ]] && OUTPUT="${OUTPUT} ${HEADSETBATT}%"
   [[ ! -z "${SINGLEBATT}" ]] && OUTPUT="${OUTPUT} ${SINGLEBATT}%"
-  [[ ! -z "${LEFTBATT}" ]] && OUTPUT="${OUTPUT} L: ${LEFTBATT}%"
+  [[ ! -z "${LEFTBATT}" ]] 	&& OUTPUT="${OUTPUT} L: ${LEFTBATT}%"
   [[ ! -z "${RIGHTBATT}" ]] && OUTPUT="${OUTPUT} R: ${RIGHTBATT}%"
-  [[ ! -z "${CASEBATT}" ]] && OUTPUT="${OUTPUT} C: ${CASEBATT}%"
+  [[ ! -z "${CASEBATT}" ]] 	&& OUTPUT="${OUTPUT} C: ${CASEBATT}%"
 
-  echo "${OUTPUT}"
+  printf "%s\\n" "${OUTPUT}"
 else
-  echo "🎧 Not Connected"
+  printf "%s Not Connected\\n" "${ICON}"
 fi
