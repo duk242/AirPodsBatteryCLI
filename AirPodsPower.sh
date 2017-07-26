15f0b1b80e51d275fcf913d5ed623c244f5ae2dc
Danoz <danoz@danoz.net>
2017-07-26 11:49:41 +1000

    Added L/R/C differentiation for percentages given More rejiggery of variables and what not

--- AirPodsPower.sh
+++ AirPodsPower.sh
@@ -3,19 +3,17 @@
 # Contributors: ankushg, spetykowski, danozdotnet
 # Check http://blog.duklabs.com/airpods-power-in-touchbar/ for more info.
 
-# Put the Mac Address of your W1-enabled headphones (Apple AirPods, Beats Solo3, Powerbeats3, BeatsX) in here.
+# Put the MAC Address of your W1-enabled headphones (Apple AirPods, Beats Solo3, Powerbeats3, BeatsX) in here.
 MACADDR='7c-04-d0-af-88-62'
-
-# Don't edit anything below this line ;)
 OUTPUT='🎧'
 VARIABLES=("BatteryPercentCombined" "HeadsetBattery" "BatteryPercentSingle" "BatteryPercentCase" "BatteryPercentLeft" "BatteryPercentRight")
-BTDATA=$(defaults read /Library/Preferences/com.apple.Bluetooth|awk '/\"${MACADDR}\".=\s*\{[^\}]*\}/i {for(i=1; i<=6; i++) {getline; print}}')
-CONNECTED=$(system_profiler SPBluetoothDataType | awk "/$MACADDR/i {for(i=1; i<=6; i++) {getline; print}}" | grep "Connected: Yes" | sed 's/.*Connected: Yes/1/')
+BTDATA=$(awk '/\"${MACADDR}\".=\s*\{[^\}]*\}/i {for(i=1; i<=6; i++) {getline; print}}'<<<defaults read /Library/Preferences/com.apple.Bluetooth)
+CONNECTED=$(awk "/$MACADDR/i {for(i=1; i<=6; i++) {getline; print}}"<<<system_profiler SPBluetoothDataType|grep "Connected: Yes"|sed 's/.*Connected: Yes/1/')
 
 if [[ "${CONNECTED}" ]]; then
   for i in "${VARIABLES[@]}"; do
-    declare -x "${i}"="$(grep "${i}" <<< "${BTDATA}"|sed "s/.*${i} = //"|sed 's/;//')"
-    [[ ! -z "${!i}" ]] && OUTPUT="${OUTPUT} ${!i}%"
+    declare -x "${i}"="$(grep "${i}"<<<"${BTDATA}"|sed "s/.*${i} = //"|sed 's/;//')"
+    [[ ! -z "${!i}" ]] && OUTPUT="${OUTPUT} $(awk '/BatteryPercent/{print substr($0,15,1)": "}'<<<${i})${!i}%"
   done
   printf "%s\\n" "${OUTPUT}"
 else
