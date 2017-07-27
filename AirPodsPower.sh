08585f9d5dc73a3491de22df26ce7287e9a7b47a
Danoz <danoz@danoz.net>
2017-07-27 14:21:31 +1000

    created a one liner version for laughs :)

--- AirPodsPower.sh
+++ AirPodsPower.sh
@@ -4,15 +4,15 @@
 # Check http://blog.duklabs.com/airpods-power-in-touchbar/ for more info.
 
 OUTPUT='🎧'; BLUETOOTH_DEFAULTS=$(defaults read /Library/Preferences/com.apple.Bluetooth); SYSTEM_PROFILER=$(system_profiler SPBluetoothDataType)
-MAC_ADDR=$(grep -b2 "Minor Type: Headphones" <<< "${SYSTEM_PROFILER}"|awk '/Address/{print $3}')
-CONNECTED=$(grep -ia6 "${MAC_ADDR}" <<< "${SYSTEM_PROFILER}" | awk '/Connected: Yes/{print 1}')
-BLUETOOTH_DATA=$(grep -ia6 '"'${MAC_ADDR}'"' <<< "${BLUETOOTH_DEFAULTS}")
+MAC_ADDR=$(grep -b2 "Minor Type: Headphones"<<<"${SYSTEM_PROFILER}"|awk '/Address/{print $3}')
+CONNECTED=$(grep -ia6 "${MAC_ADDR}"<<<"${SYSTEM_PROFILER}"|awk '/Connected: Yes/{print 1}')
+BLUETOOTH_DATA=$(grep -ia6 '"'"${MAC_ADDR}"'"'<<<"${BLUETOOTH_DEFAULTS}")
 BATTERY_LEVELS=("BatteryPercentCombined" "HeadsetBattery" "BatteryPercentSingle" "BatteryPercentCase" "BatteryPercentLeft" "BatteryPercentRight")
 
 if [[ "${CONNECTED}" ]]; then
   for I in "${BATTERY_LEVELS[@]}"; do
-    declare -x "${I}"="$(awk -v pat="${I}" '$0~pat{gsub (";",""); print $3 }'<<<"${BTDATA}")"
-    [[ ! -z "${!I}" ]] && OUTPUT="${OUTPUT} $(awk '/BatteryPercent/{print substr($0,15,1)": "}' <<< "${I}")${!I}%"
+    declare -x "${I}"="$(awk -v pat="${I}" '$0~pat{gsub (";",""); print $3 }'<<<"${BLUETOOTH_DATA}")"
+    [[ ! -z "${!I}" ]] && OUTPUT="${OUTPUT} $(awk '/BatteryPercent/{print substr($0,15,1)": "}'<<<"${I}")${!I}%"
   done
   printf "%s\\n" "${OUTPUT}"
 else
