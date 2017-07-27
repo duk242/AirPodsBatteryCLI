0bb0ad748373563994bf19b4545494557ed42763
Danoz <danoz@danoz.net>
2017-07-27 12:02:49 +1000

    updated awk to search for pattern, then print percentage. instead of having grep do the search and awk do the print. Less pipes!

--- AirPodsPower.sh
+++ AirPodsPower.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Duckie's heaps mad W1-enabled Headphone Power Script.  Version 2.2
+# Duckie's heaps mad W1-enabled Headphone Power Script.  Version 2.3
 # Contributors: ankushg, spetykowski, danozdotnet
 # Check http://blog.duklabs.com/airpods-power-in-touchbar/ for more info.
 
@@ -11,7 +11,7 @@ BATTERY_LEVELS=("BatteryPercentCombined" "HeadsetBattery" "BatteryPercentSingle"
 
 if [[ "${CONNECTED}" ]]; then
   for I in "${BATTERY_LEVELS[@]}"; do
-    declare -x "${I}"="$(grep "${I}" <<< "${BLUETOOTH_DATA}"|awk '{gsub (";",""); print $3 }')"
+    declare -x "${I}"="$(awk -v pat="${I}" '$0~pat{gsub (";",""); print $3 }'<<<"${BTDATA}")"
     [[ ! -z "${!I}" ]] && OUTPUT="${OUTPUT} $(awk '/BatteryPercent/{print substr($0,15,1)": "}' <<< "${I}")${!I}%"
   done
   printf "%s\\n" "${OUTPUT}"
